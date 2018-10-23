52c694e4f9f1f07e4cbbb9f18adb74606fc5e448
Nicklaus <enprojectxx@gmail.com>
2018-10-23 23:43:45 +0300

    added reuse

--- TrialProgram.java
+++ TrialProgram.java
@@ -10,35 +10,58 @@ public class TrialProgram {
         ReaderMeter meterValue = new ReaderMeter();
         ReaderAnswer answerValue = new ReaderAnswer();
         
-        System.out.println("Enter the number of meters ");
-        meterValue.scan();
-        meter = meterValue.incomingValue;
-        System.out.println();
+        while(true){
+            System.out.println("Enter the number of meters ");
+            meterValue.scan();
+            meter = meterValue.incomingValue;
+            System.out.println();
         
-        System.out.println("In which units you want to convert? "
-                + "1 - inches, 2 - feet, 3 - yards ");
-        answerValue.scan();
-        answer = answerValue.answerNumber;
-        
-        switch (answer) { 
-            case 1:
-                System.out.print(meter + "meters are equal = ");
-                System.out.printf("%.2f", meter*39.37);
-                System.out.println(" inches");
-                break;
-            case 2:
-                System.out.print(meter + "meters are equal = ");
-                System.out.printf("%.2f", meter*3.28);
-                System.out.println(" feet");
-                break;
-            case 3:
-                System.out.print(meter + "meters are equal = ");
-                System.out.printf("%.2f", meter*1.09);
-                System.out.println(" yards");
-                break;
-            default:
-                System.out.println("You entered an invalid value");
+            System.out.println("In which units you want to convert? "
+                    + "1 - inches, 2 - feet, 3 - yards ");
+            answerValue.scan();
+            answer = answerValue.answerNumber;
+            
+            switch (answer) { 
+                case 1:
+                    System.out.print(meter + " meters are equal = ");
+                    System.out.printf("%.2f", meter*39.37);
+                    System.out.println(" inches");
+                    break;
+                case 2:
+                    System.out.print(meter + " meters are equal = ");
+                    System.out.printf("%.2f", meter*3.28);
+                    System.out.println(" feet");
+                    break;
+                case 3:
+                    System.out.print(meter + " meters are equal = ");
+                    System.out.printf("%.2f", meter*1.09);
+                    System.out.println(" yards");
+                    break;
+                default:
+                    System.out.println("You entered an invalid value");
+                    break;
+            
+            
         }
-    
+           
+            
+            while(true){
+                System.out.println("Do you want to continue?" 
+                        + " 1 - yes , 2 - no "); 
+                answerValue.scan();
+                
+                if (answerValue.answerNumber == 1){
+                    break;
+                }
+                else if (answerValue.answerNumber == 2){
+                    System.exit(0);
+                 }
+                else {
+                    System.out.println("You entered an invalid value");
+                }
+                    
+            }
+        }
     }
 }
+    
